af88a2273f42017d0d736789992b8c2e319838ba
Julian <julian.perez-perena.ext@inetum.com>
2025-05-20 22:37:47 +0200

    Version 4. Tablas interactivas

--- Gestor_Alojamiento/FrontEnd/gestorAlojamientoFront/src/app/admin/admin.component.ts
+++ Gestor_Alojamiento/FrontEnd/gestorAlojamientoFront/src/app/admin/admin.component.ts
@@ -6,13 +6,14 @@ import { Habitacion } from '../model/habitacion.model';
 import { Establecimiento } from '../model/establecimiento.model';
 import { Usuario } from '../model/usuario.model';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class AdminComponent implements OnInit {
 
@@ -22,62 +23,162 @@ export class AdminComponent implements OnInit {
   establecimientos: Establecimiento[] = [];
   usuarios: Usuario[] = [];
 
+  // Control de visibilidad para tablas
+  mostrarReservas = false;
+  mostrarPersonas = false;
+  mostrarHabitaciones = false;
+  mostrarEstablecimientos = false;
+  mostrarUsuarios = false;
+
+  // Variables para formularios de edición/añadir
+  editandoEntidad: any = null;   // objeto actual para editar
+  entidadTipo: string = '';      // reserva, persona, habitacion, etc.
+  esNuevo: boolean = false;      // si es añadir o editar
+
   constructor(private adminService: AdminService) {}
 
-    ngOnInit(): void {
+  ngOnInit(): void {
     this.cargarDatos();
   }
 
   cargarDatos(): void {
     this.adminService.getReservas().subscribe({
-      next: (data) => {
-        this.reservas = data;
-console.log(this.reservas);
-
-      },
-      error: (err) => {
-        console.error('Error al cargar reservas:', err);
-      }
+      next: (data) => { this.reservas = data; },
+      error: (err) => { console.error('Error al cargar reservas:', err); }
     });
-
     this.adminService.getPersonas().subscribe({
-      next: (data) => {
-        this.personas = data;
-        console.log('Personas:', data);
-      },
-      error: (err) => {
-        console.error('Error al cargar personas:', err);
-      }
+      next: (data) => { this.personas = data; },
+      error: (err) => { console.error('Error al cargar personas:', err); }
     });
-
     this.adminService.getHabitaciones().subscribe({
-      next: (data) => {
-        this.habitaciones = data;
-        console.log('Habitaciones:', data);
-      },
-      error: (err) => {
-        console.error('Error al cargar habitaciones:', err);
-      }
+      next: (data) => { this.habitaciones = data; },
+      error: (err) => { console.error('Error al cargar habitaciones:', err); }
     });
-
     this.adminService.getEstablecimientos().subscribe({
-      next: (data) => {
-        this.establecimientos = data;
-        console.log('Establecimientos:', data);
-      },
-      error: (err) => {
-        console.error('Error al cargar establecimientos:', err);
-      }
+      next: (data) => { this.establecimientos = data; },
+      error: (err) => { console.error('Error al cargar establecimientos:', err); }
     });
-
     this.adminService.getUsuarios().subscribe({
-      next: (data) => {
-        this.usuarios = data;
-        console.log('Usuarios:', data);
-      },
-      error: (err) => {
-        console.error('Error al cargar usuarios:', err);
-      }
+      next: (data) => { this.usuarios = data; },
+      error: (err) => { console.error('Error al cargar usuarios:', err); }
     });
+  }
+
+  // Toggle para mostrar/ocultar tabla
+  mostrarEntidad(nombre: string) {
+    this.mostrarReservas = false;
+    this.mostrarPersonas = false;
+    this.mostrarHabitaciones = false;
+    this.mostrarEstablecimientos = false;
+    this.mostrarUsuarios = false;
+    switch(nombre) {
+      case 'reservas': this.mostrarReservas = !this.mostrarReservas; break;
+      case 'personas': this.mostrarPersonas = !this.mostrarPersonas; break;
+      case 'habitaciones': this.mostrarHabitaciones = !this.mostrarHabitaciones; break;
+      case 'establecimientos': this.mostrarEstablecimientos = !this.mostrarEstablecimientos; break;
+      case 'usuarios': this.mostrarUsuarios = !this.mostrarUsuarios; break;
+    }
+  }
+
+  // Abrir formulario para añadir
+  abrirNuevo(tipo: string) {
+    this.entidadTipo = tipo;
+    this.esNuevo = true;
+    this.editandoEntidad = {}; // objeto vacío para rellenar
+  }
+
+  // Abrir formulario para editar
+  abrirEditar(tipo: string, entidad: any) {
+    this.entidadTipo = tipo;
+    this.esNuevo = false;
+    this.editandoEntidad = { ...entidad }; // clonar objeto para no editar original directo
+  }
+
+  // Guardar cambios (añadir o editar)
+  guardar() {
+    if (this.esNuevo) {
+      this.agregarEntidad();
+    } else {
+      this.actualizarEntidad();
+    }
+  }
+
+  // Añadir entidad usando AdminService
+  agregarEntidad() {
+    switch(this.entidadTipo) {
+      case 'reservas':
+        this.adminService.addReserva(this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+      case 'personas':
+        this.adminService.addPersona(this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+      case 'habitaciones':
+        this.adminService.addHabitacion(this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+      case 'establecimientos':
+        this.adminService.addEstablecimiento(this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+      case 'usuarios':
+        this.adminService.addUsuario(this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+    }
+  }
+
+  // Actualizar entidad
+  actualizarEntidad() {
+    switch(this.entidadTipo) {
+      case 'reservas':
+        this.adminService.updateReserva(this.editandoEntidad.id, this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+      case 'personas':
+        this.adminService.updatePersona(this.editandoEntidad.dni, this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+      case 'habitaciones':
+        this.adminService.updateHabitacion(this.editandoEntidad.id, this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+      case 'establecimientos':
+        this.adminService.updateEstablecimiento(this.editandoEntidad.id, this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+      case 'usuarios':
+        this.adminService.updateUsuario(this.editandoEntidad.id, this.editandoEntidad).subscribe(() => this.postOperacion());
+        break;
+    }
+  }
+
+  // Eliminar entidad
+  eliminarEntidad(tipo: string, entidad: any) {
+    if(!confirm('¿Seguro que deseas eliminar este elemento?')) return;
+    switch(tipo) {
+      case 'reservas':
+        this.adminService.deleteReserva(entidad.id).subscribe(() => this.postOperacion());
+        break;
+      case 'personas':
+        this.adminService.deletePersona(entidad.dni).subscribe(() => this.postOperacion());
+        break;
+      case 'habitaciones':
+        this.adminService.deleteHabitacion(entidad.id).subscribe(() => this.postOperacion());
+        break;
+      case 'establecimientos':
+        this.adminService.deleteEstablecimiento(entidad.id).subscribe(() => this.postOperacion());
+        break;
+      case 'usuarios':
+        this.adminService.deleteUsuario(entidad.id).subscribe(() => this.postOperacion());
+        break;
+    }
+  }
+
+  // Refrescar datos y cerrar formulario
+  postOperacion() {
+    this.cargarDatos();
+    this.editandoEntidad = null;
+    this.entidadTipo = '';
+    this.esNuevo = false;
+  }
+
+  // Cancelar edición/añadir
+  cancelar() {
+    this.editandoEntidad = null;
+    this.entidadTipo = '';
+    this.esNuevo = false;
   }
 }
--- Gestor_Alojamiento/FrontEnd/gestorAlojamientoFront/src/app/admin/admin.service.ts
+++ Gestor_Alojamiento/FrontEnd/gestorAlojamientoFront/src/app/admin/admin.service.ts
@@ -11,28 +11,75 @@ import { Usuario } from '../model/usuario.model';
   providedIn: 'root'
 })
 export class AdminService {
-
-  private baseUrl = 'http://localhost:9020/api'; // ajusta según tu ruta backend
+  private baseUrl = 'http://localhost:9020/api';
 
   constructor(private http: HttpClient) {}
 
+  // GET
   getReservas(): Observable<Reserva[]> {
-    return this.http.get<Reserva[]>(`${this.baseUrl}/reservas`,  { withCredentials: true })
+    return this.http.get<Reserva[]>(`${this.baseUrl}/reservas`,  { withCredentials: true });
   }
-
   getPersonas(): Observable<Persona[]> {
-    return this.http.get<Persona[]>(`${this.baseUrl}/personas`, { withCredentials: true })
+    return this.http.get<Persona[]>(`${this.baseUrl}/personas`, { withCredentials: true });
   }
-
   getHabitaciones(): Observable<Habitacion[]> {
-    return this.http.get<Habitacion[]>(`${this.baseUrl}/habitaciones`,  { withCredentials: true })
+    return this.http.get<Habitacion[]>(`${this.baseUrl}/habitaciones`,  { withCredentials: true });
   }
-
   getEstablecimientos(): Observable<Establecimiento[]> {
-    return this.http.get<Establecimiento[]>(`${this.baseUrl}/establecimientos`, { withCredentials: true })
+    return this.http.get<Establecimiento[]>(`${this.baseUrl}/establecimientos`, { withCredentials: true });
+  }
+  getUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.baseUrl}/usuarios`, { withCredentials: true });
   }
 
-  getUsuarios(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(`${this.baseUrl}/usuarios`, { withCredentials: true })
+  // POST
+  addReserva(reserva: Reserva): Observable<any> {
+    return this.http.post(`${this.baseUrl}/reservas`, reserva, { withCredentials: true });
+  }
+  addPersona(persona: Persona): Observable<any> {
+    return this.http.post(`${this.baseUrl}/personas`, persona, { withCredentials: true });
+  }
+  addHabitacion(habitacion: Habitacion): Observable<any> {
+    return this.http.post(`${this.baseUrl}/habitaciones`, habitacion, { withCredentials: true });
+  }
+  addEstablecimiento(establecimiento: Establecimiento): Observable<any> {
+    return this.http.post(`${this.baseUrl}/establecimientos`, establecimiento, { withCredentials: true });
+  }
+  addUsuario(usuario: Usuario): Observable<any> {
+    return this.http.post(`${this.baseUrl}/usuarios`, usuario, { withCredentials: true });
+  }
+
+  // PUT
+  updateReserva(id: number, reserva: Reserva): Observable<any> {
+    return this.http.put(`${this.baseUrl}/reservas/${id}`, reserva, { withCredentials: true });
+  }
+  updatePersona(dni: string, persona: Persona): Observable<any> {
+    return this.http.put(`${this.baseUrl}/personas/${dni}`, persona, { withCredentials: true });
+  }
+  updateHabitacion(id: number, habitacion: Habitacion): Observable<any> {
+    return this.http.put(`${this.baseUrl}/habitaciones/${id}`, habitacion, { withCredentials: true });
+  }
+  updateEstablecimiento(id: number, establecimiento: Establecimiento): Observable<any> {
+    return this.http.put(`${this.baseUrl}/establecimientos/${id}`, establecimiento, { withCredentials: true });
+  }
+  updateUsuario(id: number, usuario: Usuario): Observable<any> {
+    return this.http.put(`${this.baseUrl}/usuarios/${id}`, usuario, { withCredentials: true });
+  }
+
+  // DELETE
+  deleteReserva(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/reservas/${id}`, { withCredentials: true });
+  }
+  deletePersona(dni: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/personas/${dni}`, { withCredentials: true });
+  }
+  deleteHabitacion(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/habitaciones/${id}`, { withCredentials: true });
+  }
+  deleteEstablecimiento(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/establecimientos/${id}`, { withCredentials: true });
+  }
+  deleteUsuario(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/usuarios/${id}`, { withCredentials: true });
   }
 }
